97323146535c11e9abcb2c4075fedfc0b1270054
ellerbus <stu.ellerbusch@gmail.com>
2025-12-26 18:11:14 -0500

    working event filter

--- tracker-app/app/Http/Controllers/Events/ListController.php
+++ tracker-app/app/Http/Controllers/Events/ListController.php
@@ -7,6 +7,8 @@ namespace App\Http\Controllers\Events;
 use App\Http\Controllers\Controller;
 use App\Models\Event;
 use App\Models\EventOrganization;
+use App\Models\Filters\EventFilter;
+use App\Models\Organization;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
@@ -29,19 +31,29 @@ class ListController extends Controller
      * @param Request $request The incoming request
      * @return View The rendered events list page with upcoming events
      */
-    public function __invoke(Request $request): View
+    public function __invoke(Request $request, EventFilter $filter): View
     {
         $with = ['organization', 'organizations' => function ($query)
         {
             $query->wherePivot(EventOrganization::CAN_ATTEND, true);
         }];
 
-        $events = Event::with($with)
+        $query = Event::with($with)
             ->withShifts()
-            ->upcoming()
-            ->get();
+            ->upcoming();
 
-        $data = compact('events');
+        $search_term = $request->query('search_term');
+
+        if ($filter->hasFilter())
+        {
+            $query = $filter->apply($query);
+        }
+
+        $events = $query->get();
+
+        $organizations = Organization::ofTypeOrganizations()->orderBy(Organization::NAME)->get();
+
+        $data = compact('events', 'search_term', 'organizations');
 
         return view('pages.events.list', $data);
     }
--- tracker-app/resources/views/pages/events/list.blade.php
+++ tracker-app/resources/views/pages/events/list.blade.php
@@ -4,40 +4,83 @@
 
 @section('content')
 
-    <div class="row p-3"
-         hx-get="{{ route('widgets.notices-htmx') }}"
+<div class="row p-3"
+     hx-get="{{ route('widgets.notices-htmx') }}"
+     hx-trigger="load"
+     hx-swap="outerHTML">
+    <div class="col text-center">
+        <x-spinner />
+    </div>
+</div>
+
+<x-card :label="'Support'">
+    <div hx-get="{{ route('widgets.support-htmx') }}"
          hx-trigger="load"
          hx-swap="outerHTML">
-        <div class="col text-center">
-            <x-spinner />
-        </div>
+        <x-loading />
     </div>
+</x-card>
 
-    <x-card :label="'Support'">
-        <div hx-get="{{ route('widgets.support-htmx') }}"
-             hx-trigger="load"
-             hx-swap="outerHTML">
-            <x-loading />
-        </div>
-    </x-card>
-
+@php($organization_id = 0)
+<form method="GET"
+      action="{{ route('events.list', ['organization_id' => $organization_id]) }}">
     <div class="row mb-3">
-        <div class="col-sm-12 col-md-6">
 
-            <input type="text"
-                   name="search_term"
-                   placeholder="Search Events"
-                   class="form-control"
-                   value="" />
+        <div class="col-sm-12 col-md-4">
+
+            @foreach (qs() as $key => $value)
+                <x-input-hidden :property="$key"
+                                :value="$value" />
+            @endforeach
+
+            <x-input-container>
+                <x-label>
+                    Search Events:
+                </x-label>
+                <x-input-text :property="'search_term'"
+                              :placeholder="'Search Event Name (at least 3 chars)'"
+                              :value="$search_term" />
+            </x-input-container>
 
         </div>
-    </div>
+        <div class="col-sm-12 col-md-4">
+            <x-input-container>
+                <x-label>
+                    Hosting Organization:
+                </x-label>
+                <x-input-picker :property="'organization_id'"
+                                :route="'pickers.organization'"
+                                :text="'Hosting Organization'"
+                                :value="-1" />
+            </x-input-container>
+            <x-modal-picker :label="'Select an Organization'" />
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <x-input-container>
+                <x-label>
+                    Character Types:
+                </x-label>
+                <x-input-select :property="'organization_id'"
+                                :options="$organizations->pluck('name', 'id')->toArray()"
+                                :value="$selected_organization->id ?? -1"
+                                :placeholder="'-- Requested Characters --'" />
+            </x-input-container>
+        </div>
+        <div class="col-12 text-end">
+            <x-submit-button>
+                Apply Filters
+            </x-submit-button>
+        </div>
 
-    <x-event-cards>
-        @foreach ($events as $event)
-            <x-event-card :event="$event" />
-        @endforeach
-    </x-event-cards>
+    </div>
+</form>
+
+
+<x-event-cards>
+    @foreach ($events as $event)
+        <x-event-card :event="$event" />
+    @endforeach
+</x-event-cards>
 
 @endsection
 
