ae7553501c7e375309f1fdbe0969b0e77f418d34
ellerbus <stu.ellerbusch@gmail.com>
2026-02-19 19:27:51 -0500

    seeder

--- tracker-app/database/seeders/FloridaGarrison/TrooperCostumeSeeder.php
+++ tracker-app/database/seeders/FloridaGarrison/TrooperCostumeSeeder.php
@@ -28,9 +28,16 @@ class TrooperCostumeSeeder extends Seeder
 
         $legacy_costumes = $this->getMappedLegacyCostumes();
 
+        $this->migrateFavoriteCostumes($club_map, $legacy_costumes);
+        $this->migrateEventTrooperCostumes($club_map, $legacy_costumes);
+    }
+
+    private function migrateFavoriteCostumes($club_map, $legacy_costumes): void
+    {
         $favorite_costumes = DB::table('favorite_costumes')
             ->join('costumes', 'costumes.id', '=', 'favorite_costumes.costumeid')
             ->whereRaw('costumes.club IS NOT NULL AND costumes.club != 4')
+            ->whereRaw("costumes.costume not in ('NA', 'N/A', 'Handler')")
             ->selectRaw('favorite_costumes.*, costumes.costume, costumes.club')
             ->get();
 
@@ -62,7 +69,45 @@ class TrooperCostumeSeeder extends Seeder
         }
     }
 
-    private function migrateTrooperCostume($club_map, $costume_id, $club_id, $favorite_costume): void
+    private function migrateEventTrooperCostumes($club_map, $legacy_costumes): void
+    {
+        $event_costumes = DB::table('event_sign_up')
+            ->join('costumes', 'costumes.id', '=', 'event_sign_up.costume')
+            ->join('troopers', 'troopers.id', '=', 'event_sign_up.trooperid')
+            ->whereRaw('costumes.club IS NOT NULL AND costumes.club != 4')
+            ->whereRaw("costumes.costume not in ('NA', 'N/A', 'Handler')")
+            ->selectRaw('distinct costumes.costume, costumes.club, costumes.id, event_sign_up.trooperid')
+            ->get();
+
+        foreach ($event_costumes as $event_costume)
+        {
+            $key = strtolower(trim($event_costume->costume));
+
+            $legacy_costume = $legacy_costumes[$key];
+
+            $is_multiple_club = array_key_exists($event_costume->club, $this->dual_club_map);
+
+            $costume_id = $legacy_costume['id'];
+
+            if ($is_multiple_club)
+            {
+                //  expand club ids from dual ids and flatten the array to
+                //  get all individual club ids for this costume
+                $club_ids = $this->expandDualClubIds($legacy_costume['clubs']);
+
+                foreach ($club_ids as $club_id)
+                {
+                    $this->migrateTrooperCostume($club_map, $costume_id, $club_id, $event_costume);
+                }
+            }
+            else
+            {
+                $this->migrateTrooperCostume($club_map, $costume_id, $event_costume->club, $event_costume);
+            }
+        }
+    }
+
+    private function migrateTrooperCostume($club_map, $costume_id, $club_id, $legacy_costume): void
     {
         $club = $club_map->firstWhere('costume_club_id', $club_id) ?? null;
 
@@ -72,7 +117,7 @@ class TrooperCostumeSeeder extends Seeder
             ->first();
 
         $trooper_costume = TrooperCostume::query()
-            ->where(TrooperCostume::TROOPER_ID, $favorite_costume->trooperid)
+            ->where(TrooperCostume::TROOPER_ID, $legacy_costume->trooperid)
             ->where(TrooperCostume::ORGANIZATION_COSTUME_ID, $org_costume->id)
             ->first();
 
@@ -80,7 +125,7 @@ class TrooperCostumeSeeder extends Seeder
         {
             $trooper_costume = new TrooperCostume();
 
-            $trooper_costume->trooper_id = $favorite_costume->trooperid;
+            $trooper_costume->trooper_id = $legacy_costume->trooperid;
             $trooper_costume->organization_costume_id = $org_costume->id;
 
             $trooper_costume->save();
