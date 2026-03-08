fdced2cd636370fd28319dbdda995caf231fa19f
ellerbus <stu.ellerbusch@gmail.com>
2026-03-08 09:17:04 -0400

    rules

--- tracker-app/database/factories/OrganizationFactory.php
+++ tracker-app/database/factories/OrganizationFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Enums\OrganizationType;
 use App\Models\Organization;
 use Database\Factories\Base\OrganizationFactory as BaseOrganizationFactory;
-use Exception;
 
 class OrganizationFactory extends BaseOrganizationFactory
 {
@@ -18,6 +17,35 @@ class OrganizationFactory extends BaseOrganizationFactory
     {
         return array_merge(parent::definition(), [
             Organization::TYPE => OrganizationType::ORGANIZATION
+        ]);
+    }
+
+    public function withName(string $name): static
+    {
+        return $this->state(fn(array $attributes): array => [
+            Organization::NAME => $name,
+        ]);
+    }
+
+    public function withNodePath(string $node_path): static
+    {
+        return $this->state(fn(array $attributes): array => [
+            Organization::NODE_PATH => $node_path,
+        ]);
+    }
+
+    public function withParent(Organization $parent): static
+    {
+        return $this->state(fn(array $attributes): array => [
+            Organization::PARENT_ID => $parent->{Organization::ID},
+            Organization::DEPTH => $parent->{Organization::DEPTH} + 1,
+        ]);
+    }
+
+    public function withIdentifierDisplay(string $identifier_display): static
+    {
+        return $this->state(fn(array $attributes): array => [
+            Organization::IDENTIFIER_DISPLAY => $identifier_display,
         ]);
     }
 }
--- tracker-app/database/factories/TrooperOrganizationFactory.php
+++ tracker-app/database/factories/TrooperOrganizationFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use App\Enums\MembershipStatus;
+use App\Models\Organization;
+use App\Models\Trooper;
 use App\Models\TrooperOrganization;
 use Database\Factories\Base\TrooperOrganizationFactory as BaseTrooperOrganizationFactory;
 
@@ -17,6 +19,27 @@ class TrooperOrganizationFactory extends BaseTrooperOrganizationFactory
     {
         return array_merge(parent::definition(), [
             TrooperOrganization::MEMBERSHIP_STATUS => MembershipStatus::ACTIVE,
+        ]);
+    }
+
+    public function forOrganization(Organization $organization): static
+    {
+        return $this->state(fn(array $attributes): array => [
+            TrooperOrganization::ORGANIZATION_ID => $organization->{Organization::ID},
+        ]);
+    }
+
+    public function forTrooper(Trooper $trooper): static
+    {
+        return $this->state(fn(array $attributes): array => [
+            TrooperOrganization::TROOPER_ID => $trooper->{Trooper::ID},
+        ]);
+    }
+
+    public function withIdentifier(string $identifier): static
+    {
+        return $this->state(fn(array $attributes): array => [
+            TrooperOrganization::IDENTIFIER => $identifier,
         ]);
     }
 }
