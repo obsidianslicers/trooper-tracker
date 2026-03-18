9dd57bdf6904134f5896a5c0e5077e180e165ae6
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2026-03-18 00:10:39 -0400

    feature: costume images display on service record costume tab

--- tracker-app/resources/views/pages/service-records/inc/trooper-costumes.blade.php
+++ tracker-app/resources/views/pages/service-records/inc/trooper-costumes.blade.php
@@ -1,6 +1,7 @@
 <x-table>
     <thead>
         <tr>
+            <th class="text-center">Image</th>
             <th>Attached Costume</th>
             <th>Organizations</th>
         </tr>
@@ -8,6 +9,20 @@
     <tbody>
         @forelse ($trooper_costumes as $trooper_costume)
             <tr>
+                <td class="text-center align-middle">
+                    @if ($trooper_costume->image_urls && $trooper_costume->image_urls->isNotEmpty())
+                        <div class="d-inline-flex gap-1 flex-wrap justify-content-center">
+                            @foreach ($trooper_costume->image_urls as $imageUrl)
+                                <div class="ratio ratio-1x1" style="width: 56px;">
+                                    <img src="{{ $imageUrl }}"
+                                         alt="{{ $trooper_costume->name }} costume image"
+                                         class="img-fluid rounded border"
+                                         style="object-fit: cover;" />
+                                </div>
+                            @endforeach
+                        </div>
+                    @endif
+                </td>
                 <td class="text-nowrap">
                     {{ $trooper_costume->name }}
                 </td>
@@ -16,7 +31,7 @@
                 </td>
             </tr>
         @empty
-            <x-table-empty :colspan="2">
+            <x-table-empty :colspan="3">
                 @if(!$trooper->is_handler)
                     No Attached Costumes ... Yet!
                 @endif
