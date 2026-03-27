92f5c18e2e352cb4e8c739028fef4d997e10617a
ellerbus <stu.ellerbusch@gmail.com>
2026-03-27 13:52:41 -0400

    ui

--- tracker-app/resources/js/auth/index.js
+++ tracker-app/resources/js/auth/index.js
@@ -1,7 +1,9 @@
 import organizationSelector from './organizationSelector';
+import registration from './registration';
 
 export default function (Alpine) {
     window.Auth = window.Auth || {};
     window.Auth.Register = window.Auth.Register || {};
     window.Auth.Register.organizationSelector = organizationSelector;
+    window.Auth.Register.registration = registration;
 }
---
+++ tracker-app/resources/js/auth/registration.js
@@ -0,0 +1,20 @@
+export default function () {
+    return {
+        activeOrganizationIds: [],
+
+        // Computed property to check if any active org requires a guardian
+        get requiresGuardian() {
+            return window.$organization_hierarchy
+                .filter(org => this.activeOrganizationIds.includes(org.id))
+                .some(org => org.requires_guardian);
+        },
+
+        handleOrgToggled(id, isActive) {
+            if (isActive) {
+                if (!this.activeOrganizationIds.includes(id)) this.activeOrganizationIds.push(id);
+            } else {
+                this.activeOrganizationIds = this.activeOrganizationIds.filter(orgId => orgId !== id);
+            }
+        }
+    }
+}
