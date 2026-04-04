2ff0bc93364cedd748aaddcbcedc7c2997ecf895
Stu Ellerbusch <ellerbus@users.noreply.github.com>
2026-04-04 16:14:28 -0400

    chore: update event display ui on limits

--- tracker-app/resources/views/pages/events/inc/attendance.blade.php
+++ tracker-app/resources/views/pages/events/inc/attendance.blade.php
@@ -3,25 +3,29 @@
     <li class="list-group-item bg-transparent border-bottom">
         Expected Attendees
         <span class="float-end">
-            <x-number-format :value="$event->expected_attendees" />
+            <x-number-format :value="$event->expected_attendees"
+                             :nulldisplay="'alot'" />
         </span>
     </li>
     <li class="list-group-item bg-transparent border-bottom">
         Maximum Shifts Allowed
         <span class="float-end">
-            <x-number-format :value="$event->shifts_allowed" />
+            <x-number-format :value="$event->shifts_allowed"
+                             :nulldisplay="'all'" />
         </span>
     </li>
     <li class="list-group-item bg-transparent border-bottom">
         Troopers Allowed
         <span class="float-end">
-            <x-number-format :value="$event->troopers_allowed" />
+            <x-number-format :value="$event->troopers_allowed"
+                             :nulldisplay="'alot'" />
         </span>
     </li>
     <li class="list-group-item bg-transparent border-bottom">
         Handlers Allowed
         <span class="float-end">
-            <x-number-format :value="$event->handlers_allowed" />
+            <x-number-format :value="$event->handlers_allowed"
+                             :nulldisplay="'alot'" />
         </span>
     </li>
 </ul>
