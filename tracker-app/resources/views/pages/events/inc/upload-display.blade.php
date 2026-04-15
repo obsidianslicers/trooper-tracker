c3d737fcacd8957f2d6ca1b49522e5d8c78f413e
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2026-04-15 12:55:02 -0400

    PR

--- tracker-app/resources/views/pages/events/inc/upload-display.blade.php
+++ tracker-app/resources/views/pages/events/inc/upload-display.blade.php
@@ -1,11 +1,10 @@
-<div id="event-uploads">
+@php $section_id = $is_administrative ? 'admin-event-uploads' : 'event-uploads'; @endphp
+<div id="{{ $section_id }}">
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
         @foreach($event->event_uploads as $event_upload)
             @if($event_upload->is_administrative == $is_administrative)
                 @php
-                    $isTaggedForMe = $event_upload->relationLoaded('troopers')
-                        ? $event_upload->troopers->contains('id', auth()->id())
-                        : $event_upload->troopers()->where('tt_troopers.id', auth()->id())->exists();
+                    $isTaggedForMe = $event_upload->troopers->contains('id', auth()->id());
                 @endphp
 
                 <div class="col">
@@ -17,8 +16,8 @@
                             {!! Share::page(route('shares.event-upload', compact('event', 'event_upload')), $event->name)->facebook()->twitter() !!}
 
                             <form hx-post="{{ route('events.toggle-upload-tag', compact('event_upload')) }}"
-                                  hx-select="#event-uploads"
-                                  hx-target="#event-uploads"
+                                  hx-select="#{{ $section_id }}"
+                                  hx-target="#{{ $section_id }}"
                                   hx-swap="outerHTML"
                                   hx-indicator="#transmission-bar-upload-images"
                                   class="mt-2">
