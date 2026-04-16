c56859fc7989d513f4ddff1af5bb87ab74c881cb
Stu Ellerbusch <ellerbus@users.noreply.github.com>
2026-04-16 14:14:35 -0400

    fix: email template (#105)

--- tracker-app/resources/views/emails/events/share-event-roster.blade.php
+++ tracker-app/resources/views/emails/events/share-event-roster.blade.php
@@ -2,28 +2,28 @@
 
 @section('message')
 
-<p>
-    Hello,
-</p>
+    <p>
+        Hello,
+    </p>
 
-<p>
-    Thank you for coordinating the upcoming event. Our command staff has prepared a
-    public-facing roster for your reference.
-</p>
+    <p>
+        Thank you for coordinating the upcoming event. Our command staff has prepared a
+        public-facing roster for your reference.
+    </p>
 
-<p>
-    You can access it here:
-    <a href="{{ route('shares.roster', $event_share) }}"></a>
-</p>
+    <p>
+        You can access it here:
+        <a href="{{ route('shares.roster', $event_share) }}">link</a>
+    </p>
 
-<p>
-    This roster is intended for your use in coordinating with your team and volunteers.
-    Please do not share the link publicly.
-</p>
+    <p>
+        This roster is intended for your use in coordinating with your team and volunteers.
+        Please do not share the link publicly.
+    </p>
 
-<p>
-    Best regards,<br />
-    {{ $trooper->legal_name }}<br />
-    Trooper Command Staff / Administration <br />
-</p>
+    <p>
+        Best regards,<br />
+        {{ $trooper->legal_name }}<br />
+        Trooper Command Staff / Administration <br />
+    </p>
 @endsection
