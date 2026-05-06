e8c1173624d2b736df59bc4fb6219e2df8f9c495
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2026-05-06 16:39:44 -0400

    notificaiton bell fix

--- tracker-app/resources/views/partials/inc/navbar-notifications.blade.php
+++ tracker-app/resources/views/partials/inc/navbar-notifications.blade.php
@@ -1,14 +1,17 @@
 @auth
     <li class="nav-item">
-        <a class="nav-link position-relative {{ request()->routeIs('account.push-notifications') ? 'active' : '' }}"
+        <a class="nav-link {{ request()->routeIs('account.push-notifications') ? 'active' : '' }}"
            href="{{ route('account.push-notifications') }}">
-            <i class="fa fa-fw fa-bell"></i>
-            @if($pushNotificationUnreadCount > 0)
-                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-                      style="font-size: 0.6rem;">
-                    {{ $pushNotificationUnreadCount > 99 ? '99+' : $pushNotificationUnreadCount }}
-                </span>
-            @endif
+            <span class="position-relative">
+                <i class="fa fa-fw fa-bell"></i>
+                @if($pushNotificationUnreadCount > 0)
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+                          style="font-size: 0.6rem;">
+                        {{ $pushNotificationUnreadCount > 99 ? '99+' : $pushNotificationUnreadCount }}
+                    </span>
+                @endif
+            </span>
+            <span class="d-md-none ms-1">Notifications</span>
         </a>
     </li>
 @endauth
