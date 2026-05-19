ccaf3487e0eaa7e5f9fbbcdbfe493d60cd33d7ea
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2026-05-19 17:27:29 -0400

    Update AdminDisplayController.php

--- tracker-app/app/Http/Controllers/Admin/AdminDisplayController.php
+++ tracker-app/app/Http/Controllers/Admin/AdminDisplayController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\MembershipStatus;
 use App\Http\Controllers\MagicBusController;
 use App\Models\Trooper;
 use App\Models\TrooperOrganization;
@@ -33,7 +34,10 @@ class AdminDisplayController extends MagicBusController
 
         $not_approved = Trooper::pendingApprovals()->moderatedBy($trooper)->count();
 
-        $pending_join_requests = TrooperOrganization::pending()->forModerator($trooper)->count();
+        $pending_join_requests = TrooperOrganization::pending()
+            ->whereHas('trooper', fn ($q) => $q->where(Trooper::MEMBERSHIP_STATUS, '!=', MembershipStatus::PENDING))
+            ->forModerator($trooper)
+            ->count();
 
         $this->buildApprovalFlashMessages($not_approved, $pending_join_requests);
 
