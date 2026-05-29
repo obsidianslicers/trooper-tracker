7d8210d76357046eb1699da8cc6dad3e2745bbae
ellerbus <stu.ellerbusch@gmail.com>
2026-05-29 18:58:10 -0400

    ui

--- tracker-app/app/Features/Events/Queries/GetEventShiftDisplayQueryHandler.php
+++ tracker-app/app/Features/Events/Queries/GetEventShiftDisplayQueryHandler.php
@@ -9,6 +9,7 @@ use App\Models\Costume;
 use App\Models\EventGuest;
 use App\Models\EventShift;
 use App\Models\EventTrooper;
+use App\Models\Organization;
 use App\Models\Trooper;
 
 /**
@@ -45,11 +46,19 @@ readonly class GetEventShiftDisplayQueryHandler implements QueryHandlerInterface
 
         $this->assembleEventShift($event_shift, $message->trooper);
 
+        $this->transformEventShift($event_shift);
+
         return $event_shift;
     }
 
     private function buildRelations(): array
     {
+        $organization_columns = [
+            Organization::ID,
+            Organization::NAME,
+            Organization::IMAGE_PATH_SM,
+        ];
+
         $trooper_columns = [
             Trooper::ID,
             Trooper::LEGAL_NAME,
@@ -64,6 +73,12 @@ readonly class GetEventShiftDisplayQueryHandler implements QueryHandlerInterface
 
         return [
             'event',
+            'event.organization:' . implode(',', $organization_columns),
+            'event.organizations.organization',
+            'event.organizations' => function ($query)
+            {
+                $query->orderBy(Organization::NAME);
+            },
             'event_troopers.trooper:' . implode(',', $trooper_columns),
             'event_troopers.trooper.trooper_costumes.organization_costume',
             'event_troopers.costume:' . implode(',', $costume_columns),
