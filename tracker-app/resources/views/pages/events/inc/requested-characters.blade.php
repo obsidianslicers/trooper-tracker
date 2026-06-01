511017865cd26913872a0d2a32e49722fe5bffc9
ellerbus <stu.ellerbusch@gmail.com>
2026-06-01 17:12:54 -0400

    nit

--- tracker-app/resources/views/pages/events/inc/requested-characters.blade.php
+++ tracker-app/resources/views/pages/events/inc/requested-characters.blade.php
@@ -1,9 +1,7 @@
 <x-section-title>Requested Characters</x-section-title>
-@if($event->requested_number_characters && $event->requested_number_characters > 0)
-    <p>
-        Number of Characters: {{ $event->requested_number_characters }}
-    </p>
-@endif
+<p>
+    Number of Characters: {{ $event->requested_number_characters ?? 'not specified' }}
+</p>
 <p>
     Character Types: {{ $event->requested_character_types ?? 'not specified' }}
 </p>
