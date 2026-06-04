7dcdfebb0e268ea4adc36feedcc5814c29f90bb5
ellerbus <stu.ellerbusch@gmail.com>
2026-06-04 10:23:00 -0400

    intertia config

--- tracker-app/config/inertia.php
+++ tracker-app/config/inertia.php
@@ -7,7 +7,7 @@ return [
         'url' => env('INERTIA_SSR_URL', 'http://127.0.0.1:13714'),
         'ensure_bundle_exists' => (bool) env('INERTIA_SSR_ENSURE_BUNDLE_EXISTS', false),
     ],
-    'ensure_pages_exist' => false,
+    'ensure_pages_exist' => true,
     'page_paths' => [
         resource_path('svelte/pages'),
     ],
