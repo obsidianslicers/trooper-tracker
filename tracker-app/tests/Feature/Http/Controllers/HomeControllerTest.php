62db48982a847d9a0f92cc72d12afd21b4415543
ellerbus <stu.ellerbusch@gmail.com>
2026-06-06 09:08:43 -0400

    fix tests

--- tracker-app/tests/Feature/Http/Controllers/HomeControllerTest.php
+++ tracker-app/tests/Feature/Http/Controllers/HomeControllerTest.php
@@ -6,18 +6,31 @@ namespace Tests\Feature\Http\Controllers;
 
 use App\Models\Trooper;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia as Assert;
 use Tests\TestCase;
 
 class HomeControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_invoke_displays_home_page_for_guests(): void
+    public function test_invoke_displays_blade_home_page_for_guests_when_debug_is_disabled(): void
     {
+        config(['app.debug' => false]);
+
         $response = $this->get(route('home'));
 
         $response->assertOk();
         $response->assertViewIs('pages.home');
+    }
+
+    public function test_invoke_displays_inertia_home_page_for_guests_when_debug_is_enabled(): void
+    {
+        config(['app.debug' => true]);
+
+        $response = $this->get(route('home'));
+
+        $response->assertOk();
+        $response->assertInertia(fn(Assert $page): Assert => $page->component('Home'));
     }
 
     public function test_invoke_redirects_authenticated_troopers_to_events_list(): void
