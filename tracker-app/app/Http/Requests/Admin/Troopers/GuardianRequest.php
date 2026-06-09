086983936d41fa89b060f4a830ac0ed3fae71ef0
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2026-06-09 07:47:44 -0400

    Update GuardianRequest.php

--- tracker-app/app/Http/Requests/Admin/Troopers/GuardianRequest.php
+++ tracker-app/app/Http/Requests/Admin/Troopers/GuardianRequest.php
@@ -7,6 +7,7 @@ namespace App\Http\Requests\Admin\Troopers;
 use App\Models\Trooper;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Carbon;
 use Illuminate\Validation\Rule;
 
 class GuardianRequest extends FormRequest
@@ -25,10 +26,13 @@ class GuardianRequest extends FormRequest
 
     public function rules(): array
     {
+        $dob = $this->input(Trooper::DATE_OF_BIRTH);
+        $is_minor = !empty($dob) && Carbon::parse($dob)->age < 18;
+
         return [
             Trooper::DATE_OF_BIRTH => ['nullable', 'date'],
             'guardian_email' => [
-                'nullable',
+                $is_minor ? 'required' : 'nullable',
                 'email',
                 'max:256',
                 Rule::exists('tt_troopers', Trooper::EMAIL),
