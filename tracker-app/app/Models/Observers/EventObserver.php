930625a04e32a7b0070504b4c32af8d1ac2a54bb
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2026-08-07 21:55:21 -0400

    fix: trim(): Argument #1 ($string) must be of type string, null given (#400)

--- tracker-app/app/Models/Observers/EventObserver.php
+++ tracker-app/app/Models/Observers/EventObserver.php
@@ -127,7 +127,7 @@ class EventObserver
 
         try
         {
-            $address = $event->venue_address;
+            $address = $event->venue_address ?? '';
 
             if (empty(trim($address)))
             {
