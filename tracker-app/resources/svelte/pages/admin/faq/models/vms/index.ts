5aa75b23cc43b785f2d605bf60f7006a3f3a04ba
ellerbus <stu.ellerbusch@gmail.com>
2026-09-07 09:42:32 -0400

    ts cleanup

--- tracker-app/resources/svelte/pages/admin/faq/models/vms/index.ts
+++ tracker-app/resources/svelte/pages/admin/faq/models/vms/index.ts
@@ -1,7 +1,6 @@
 
 export * from './CreateItemViewModel.svelte';
 export * from './CreateSectionViewModel.svelte';
-export * from './FaqFormViewModel.svelte';
 export * from './IndexViewModel.svelte';
 export * from './UpdateItemViewModel.svelte';
 export * from './UpdateSectionViewModel.svelte';
