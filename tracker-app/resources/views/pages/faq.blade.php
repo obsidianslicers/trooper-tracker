2d70b15717d06d6c66f72a05454b7e3c5f23d444
ellerbus <stu.ellerbusch@gmail.com>
2026-09-10 05:53:34 -0400

    route update

--- tracker-app/resources/views/pages/faq.blade.php
+++ tracker-app/resources/views/pages/faq.blade.php
@@ -77,7 +77,7 @@
                                 @auth
                                     @if(Auth::user()->is_administrator)
                                         <div class="mt-2 pt-2 border-top border-secondary d-flex justify-content-end">
-                                            <a href="{{ route('admin.faq.update', $item) }}"
+                                            <a href="{{ route('admin.faq.items.update', $item) }}"
                                                class="btn btn-sm btn-outline-warning">
                                                 <i class="fa fa-fw fa-edit me-1"></i> Edit
                                             </a>
