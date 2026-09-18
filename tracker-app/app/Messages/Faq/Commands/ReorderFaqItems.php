4d5caea3e94afd619128b6f935227197edb23d3e
ellerbus <stu.ellerbusch@gmail.com>
2026-09-18 13:20:26 -0400

    pint

--- tracker-app/app/Messages/Faq/Commands/ReorderFaqItems.php
+++ tracker-app/app/Messages/Faq/Commands/ReorderFaqItems.php
@@ -7,7 +7,6 @@ namespace App\Messages\Faq\Commands;
 use App\Models\Faq;
 use Hyperdrive\Concerns\ShouldBeTransactional;
 use Hyperdrive\Message;
-use Illuminate\Support\Facades\DB;
 
 /**
  * @method static void call(array $ordered_ids)
@@ -18,8 +17,7 @@ final class ReorderFaqItems extends Message
 
     public function __construct(
         private readonly array $ordered_ids,
-    ) {
-    }
+    ) {}
 
     public function handle(): void
     {
