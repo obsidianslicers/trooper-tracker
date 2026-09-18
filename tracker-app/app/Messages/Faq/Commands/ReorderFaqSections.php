a105be1e4abd8a9a9332b3cce75abd514aa21bd8
ellerbus <stu.ellerbusch@gmail.com>
2026-09-18 13:18:19 -0400

    ehance messages

--- tracker-app/app/Messages/Faq/Commands/ReorderFaqSections.php
+++ tracker-app/app/Messages/Faq/Commands/ReorderFaqSections.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Messages\Faq\Commands;
 
 use App\Models\FaqSection;
+use Hyperdrive\Concerns\ShouldBeTransactional;
 use Hyperdrive\Message;
 use Illuminate\Support\Facades\DB;
 
@@ -13,19 +14,20 @@ use Illuminate\Support\Facades\DB;
  */
 final class ReorderFaqSections extends Message
 {
+    use ShouldBeTransactional;
+
     public function __construct(
         private readonly array $ordered_ids,
-    ) {}
+    ) {
+    }
 
     public function handle(): void
     {
-        DB::transaction(function () {
-            foreach ($this->ordered_ids as $position => $id)
-            {
-                FaqSection::query()
-                    ->where(FaqSection::ID, (int) $id)
-                    ->update([FaqSection::SORT_ORDER => $position + 1]);
-            }
-        });
+        foreach ($this->ordered_ids as $position => $id)
+        {
+            FaqSection::query()
+                ->where(FaqSection::ID, (int) $id)
+                ->update([FaqSection::SORT_ORDER => $position + 1]);
+        }
     }
 }
